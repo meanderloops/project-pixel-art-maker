8662bf6821dd6099871d9689e5e23058be9138bc
Coby-McGraw <mcgraw.coby@gmail.com>
2022-05-02 13:34:47 -0500

    Adds variables

--- designs.js
+++ designs.js
@@ -1,7 +1,12 @@
 // Select color input
+var pixelColor = document.getElementById("colorPicker");
+
 // Select size input
+var gridHeight = document.getElementById("inputHeight");
+var gridWidth = document.getElementById("inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+const gridSubmit = document.querySelector("input[type=submit]");
 
 function makeGrid() {
 
