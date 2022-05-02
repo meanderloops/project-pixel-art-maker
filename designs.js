4a6e0be2471946a0b5104b0706cd3ae7a3685ac8
Coby-McGraw <mcgraw.coby@gmail.com>
2022-05-02 14:49:57 -0500

    Adds working code to makeGrid function

--- designs.js
+++ designs.js
@@ -6,10 +6,24 @@ var gridHeight = document.getElementById("inputHeight");
 var gridWidth = document.getElementById("inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+const pixelCanvas = document.getElementById("pixelCanvas");
 const gridSubmit = document.querySelector("input[type=submit]");
 
 function makeGrid() {
 
 // Your code goes here!
+console.log('grid is ' + gridHeight.value + ' x ' + gridWidth.value);
+
+var html = ""; 
+
+for(var i =0; i <= (gridHeight.value - 1); i++) { 
+    html += '<tr>'; 
+    for(var h=0; h<= (gridWidth.value - 1); h++) { 
+       html += '<td></td>'; 
+    } 
+    html += '</tr>'; 
+}
+
+pixelCanvas.innerHTML += html;
 
 }
